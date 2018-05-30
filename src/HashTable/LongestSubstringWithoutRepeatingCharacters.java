3efb05ef3a68f078b185d7914042bf3ed3d43281
Roger <zherogerluo@gmail.com>
2018-05-29 23:56:16 -0500

    Problems 2 and 3

---
+++ src/HashTable/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,70 @@
+/**
+ * LeetCode #3, medium
+ *
+ * Given a string, find the length of the longest substring without repeating characters.
+
+ Examples:
+
+ Given "abcabcbb", the answer is "abc", which the length is 3.
+
+ Given "bbbbb", the answer is "b", with the length of 1.
+
+ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+
+package HashTable;
+
+import java.util.*;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    /**
+     * Solution 1: Hash map
+     *
+     * Traverse string and use a hash map to record visited character and its last index, AND a pointer to record
+     * starting index of current substring. If current character is visited, AND its last appearance is in this
+     * substring, update the starting index. Within the loop, update the hash map, and update the result based on
+     * the starting index of current substring.
+     *
+     * Tricky part: Need to remember the starting point of current substring, need to check if a visited character
+     * is within this substring (start is max of start and index+1 checked out from hash map).
+     *
+     * Other thoughts: Use array to replace hash map for better performance, if the character set is know
+     * (ASCII for example, use int[256])
+     */
+    class Solution1 {
+        public int lengthOfLongestSubstring(String s) {
+            Map<Character, Integer> map = new HashMap<>();
+            int res = 0, start = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (map.containsKey(c)) start = Math.max(start, map.get(c) + 1); // tricky part
+                res = Math.max(res, i - start + 1);
+                map.put(c, i);
+            }
+            return res;
+        }
+    }
+
+    /**
+     * Solution 2: Hash map
+     *
+     * Slightly modified from Solution 1: If a character was already visited, remove everything before it (inclusive).
+     * Performance hit because of more hash map operations.
+     */
+    class Solution2 {
+        public int lengthOfLongestSubstring(String s) {
+            Map<Character, Integer> map = new HashMap<>();
+            int res = 0, start = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (map.containsKey(c)) {
+                    res = Math.max(res, i - start);
+                    int k = map.get(c);
+                    while (start <= k) map.remove(s.charAt(start++));
+                }
+                map.put(c, i);
+            }
+            return Math.max(res, s.length() - start);
+        }
+    }
+}
---
+++ src/LinkedList/AddTwoNumbers.java
@@ -0,0 +1,52 @@
+/**
+ * LeetCode #2, medium
+ *
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+ You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ Example
+
+ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ Output: 7 -> 0 -> 8
+ Explanation: 342 + 465 = 807.
+ */
+
+package LinkedList;
+
+public class AddTwoNumbers {
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+    }
+
+    /**
+     * Solution 1: Linked list traversal
+     *
+     * Traverse two linked lists at the same time, keeping record of the carry digit (0 or 1).
+     *
+     * Tricky part: one list node becomes null (check for nulls), BOTH list nodes become null
+     * BUT carry digit is not zero. In these cases the loop should still go through. The ending
+     * condition is that, both lists reach end AND carry digit is zero.
+     */
+    class Solution1 {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode sentinel = new ListNode(0), prev = sentinel;
+            int carry = 0;
+            while (l1 != null || l2 != null || carry != 0) { // note the carry digit condition
+                int val = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+                carry = val / 10;
+                val = val % 10;
+                prev.next = new ListNode(val);
+                prev = prev.next;
+                if (l1 != null) l1 = l1.next; // check for null!
+                if (l2 != null) l2 = l2.next; // check for null!
+            }
+            return sentinel.next;
+        }
+    }
+}
