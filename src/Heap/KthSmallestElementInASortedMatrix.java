7b38d4ba1b669c0b07d7765fb6ee8fa0c3022b2b
Roger <zherogerluo@gmail.com>
2018-08-19 21:33:37 -0500

    Binary search solution for Problem 378

--- src/Heap/KthSmallestElementInASortedMatrix.java
+++ src/Heap/KthSmallestElementInASortedMatrix.java
@@ -35,7 +35,7 @@ public class KthSmallestElementInASortedMatrix {
      * that more cache hits can be achieved (not sure though). Or, use whichever is shorter so that heap size can be
      * minimized?
      *
-     * Time complexity: O(k * log(m)). Space complexity: O(m).
+     * Time complexity: O(k * log(n)). Space complexity: O(n).
      */
     class Solution1 {
         public int kthSmallest(int[][] matrix, int k) {
@@ -55,6 +55,42 @@ public class KthSmallestElementInASortedMatrix {
 
     /**
      * Solution 2: Binary search
+     *
+     * Binary search on values. We calculate mid value based on low and high, and count the number of entries smaller
+     * or equal to it. If count < k, we update low, otherwise update high. Every time we cut the searching range by
+     * half, and for each search, there is a clever trick to do it in O(n). When searching row by row from first to
+     * last, we can certainly do a binary search for mid for every row, which would lead to O(n * log(n)) time.
+     * However, note that the numbers are sorted not only in rows, but also in columns. This means that, if we find
+     * mid and position k for this row, we don't have to search for index > k in the next row, because the number at
+     * k in next row would be larger than that of this row for sure. In this case, we can just keep a single index
+     * marking the right-hand-side boundary of our search, and while we go from top row to bottom row, this index
+     * would only shrink, so the entire search would be completed when the index goes to 0, i.e. finished in O(n) time.
+     *
+     * Another trick thing to note is that, how is the return value low guaranteed to be the value in the matrix? It
+     * relies on the precondition that such kth smallest element exists. If low is smaller than target, the count
+     * would always be smaller than k, and low would keep incrementing until it sits on the kth smallest element and
+     * won't go beyond that, and the binary search loop ends when high goes below low.
+     *
+     * Time complexity: O(n * log(max - min)), which is effectively O(n) because the range of int is a constant, thus
+     * the log value. Space complexity: O(1).
      */
-    // TODO
+    class Solution2 {
+        public int kthSmallest(int[][] matrix, int k) {
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0 || k <= 0)
+                return Integer.MIN_VALUE;
+            final int m = matrix.length, n = matrix[0].length;
+            int low = matrix[0][0], high = matrix[m-1][n-1];
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                int count = 0;
+                for (int i = 0, j = n-1; i < m && j >= 0; i++) {
+                    while (j >= 0 && matrix[i][j] > mid) j--;
+                    count += j + 1;
+                }
+                if (count < k) low = mid + 1;
+                else high = mid - 1;
+            }
+            return low;
+        }
+    }
 }
