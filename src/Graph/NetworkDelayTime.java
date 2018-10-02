4440e87cb35356d0fc1118005038d7ae5ebc1958
Roger <zherogerluo@gmail.com>
2018-10-01 22:21:36 -0500

    Problem 743: Dijkstra

--- src/Graph/NetworkDelayTime.java
+++ src/Graph/NetworkDelayTime.java
@@ -36,6 +36,7 @@ public class NetworkDelayTime {
             int[] dist = new int[N+1];
             Arrays.fill(dist, Integer.MAX_VALUE);
             dist[K] = 0;
+            dist[0] = 0; // zeroth node will not be used, set to zero to avoid bug in later max search
             for (int i = 1; i < N; i++) {
                 boolean changed = false;
                 for (int[] time : times) {
@@ -48,7 +49,46 @@ public class NetworkDelayTime {
                 if (!changed) break;
             }
             int max = 0;
-            dist[0] = 0;
+            for (int d : dist) {
+                if (d == Integer.MAX_VALUE) return -1;
+                max = Math.max(max, d);
+            }
+            return max;
+        }
+    }
+
+    /**
+     * Solution 2: Dijkstra's algorithm
+     *
+     * Heap-based Dijkstra's. Instead of using a modifiable heap, just abandon heap nodes with longer distance than
+     * recorded (outdated entry). There are few tricky lines that can easily be mistaken, see comments below.
+     *
+     * Time complexity: O((E + V) * log(V)). Space complexity: O(E + V).
+     */
+    class Solution2 {
+        public int networkDelayTime(int[][] times, int N, int K) {
+            List<int[]>[] graph = new List[N+1]; // list of edges for node i
+            for (int[] time : times) {
+                if (graph[time[0]] == null) graph[time[0]] = new ArrayList<>();
+                graph[time[0]].add(time);
+            }
+            int[] dist = new int[N+1];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            dist[0] = 0; // don't set dist[K] to zero, otherwise won't populate its neighbors!
+            Queue<int[]> heap = new PriorityQueue<>((a, b) -> a[1] - b[1]); // [node, distance]
+            heap.offer(new int[]{K, 0});
+            while (!heap.isEmpty()) {
+                int[] pair = heap.poll();
+                int curNode = pair[0], curDist = pair[1];
+                if (dist[curNode] <= curDist) continue; // skip those outdated entries in heap
+                dist[curNode] = curDist;
+                if (graph[curNode] == null) continue; // necessary for leaf nodes!
+                for (int[] edge : graph[curNode]) {
+                    int next = edge[1], weight = edge[2];
+                    heap.offer(new int[]{next, curDist + weight});
+                }
+            }
+            int max = 0;
             for (int d : dist) {
                 if (d == Integer.MAX_VALUE) return -1;
                 max = Math.max(max, d);
